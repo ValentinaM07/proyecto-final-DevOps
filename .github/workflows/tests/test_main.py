6fb7bad57921b05fb629fea2e9986b44f338ced6
ValentinaM07 <vaed107@gmail.com>
2025-07-19 00:22:37 -0500

    Actualizacion

--- .github/workflows/tests/test_main.py
+++ .github/workflows/tests/test_main.py
@@ -1,8 +1,12 @@
 from fastapi.testclient import TestClient
-from run import app  # Asegúrate que 'run.py' contenga tu instancia FastAPI
+from run import app  # Asegúrate que tu app esté en run.py
 
 client = TestClient(app)
 
-def test_read_root():
+def test_root():
     response = client.get("/")
+    assert response.status_code == 200
+
+def test_metrics():
+    response = client.get("/metrics")
     assert response.status_code == 200
